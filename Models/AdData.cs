5aaefe839a2c5b51cc3389e119fb58b3c753130e
rolcho <roland@cimem.hu>
2024-09-06 11:53:41 +0200

    change styles

--- Models/AdData.cs
+++ Models/AdData.cs
@@ -10,7 +10,7 @@ public class AdData
     public static readonly Dictionary<AdType, string> Styles =
         new()
         {
-            { AdType.Plain, "text-black p-y-4 border-b-2 border-black w-full" },
+            { AdType.Plain, "text-black py-4 border-b-2 border-black w-full" },
             { AdType.HighlightBackground, "bg-red-600 text-white p-4 w-full" },
             { AdType.YellowBackground, "text-black p-4 bg-yellow-400 w-full" },
             { AdType.BlackBackground, "text-white p-4 bg-black w-full" },
@@ -21,7 +21,7 @@ public class AdData
                 AdType.Header,
                 "p-4 bg-red-600 text-white w-full text-center uppercase border-b-4 border-black w-full"
             },
-            { AdType.Circle, "p-6 border-4 border-pink-600 rounded-full w-full" },
+            { AdType.Circle, "px-6 py-4 border-4 border-violet-600 rounded-large w-full" },
             { AdType.LightRedBackground, "text-black p-4 bg-red-200 w-full" },
             { AdType.Error, "text-red-600 p-4 w-full" },
         };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+/* eslint-disable */
+/* prettier-ignore */
 module.exports = {
   content: ["./**/*.{razor,html,cs}"],
   theme: {
@@ -6,6 +8,16 @@ module.exports = {
       fontFamily: {
         sans: ["Roboto", "ui-sans-serif", "system-ui"],
       },
+    },
+    borderRadius: {
+      'none': '0',
+      'sm': '0.125rem',
+      DEFAULT: '4px',
+      'md': '0.375rem',
+      'lg': '0.5rem',
+      'xl': '0.75rem',
+      'full': '9999px',
+      'large': '40px',
     },
   },
   plugins: [],
